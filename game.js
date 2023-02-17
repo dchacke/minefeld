52344d1dbaf675a046c654089ee896e3dd2fdde6
Dennis C Hackethal <dennis.hackethal@protonmail.com>
2023-02-16 19:47:06 -0700

    Highlight grid of surrounding tiles on hover

--- game.js
+++ game.js
@@ -1,9 +1,27 @@
 let $tiles = document.getElementById('tiles');
+let rows = 9;
+let columns = 9;
+
+let getSurrounding = $tile => {
+  let row = Number($tile.dataset.row);
+  let column = Number($tile.dataset.column);
+
+  let min = 0;
+  let maxRow = rows - 1;
+  let maxColumn = columns - 1;
+
+  let affectedRows = Array.from(new Set([Math.max(min, row - 1), row, Math.min(maxRow, row + 1)]));
+  let affectedColumns = Array.from(new Set([Math.max(min, column - 1), column, Math.min(maxColumn, column + 1)]));
+
+  return affectedRows
+    .map(i => {
+      return affectedColumns
+        // .filter(j => i != row && j != column)
+        .map(j => document.getElementById(`tile_${i}_${j}`));
+    }).flat();
+};
 
 let createTiles = () => {
-  let rows = 9;
-  let columns = 9;
-
   for (let i = 0; i < rows; i++) {
     let $row = document.createElement('div');
 
@@ -13,10 +31,13 @@ let createTiles = () => {
       let $tile = document.createElement('div');
 
       $tile.classList.add('tile');
+      $tile.id = `tile_${i}_${j}`;
 
       let isMine = Math.random() > 0.9;
 
       $tile.dataset.isMine = isMine;
+      $tile.dataset.row = i;
+      $tile.dataset.column = j;
 
       if (isMine) {
         $tile.innerHTML = 'x';
@@ -25,9 +46,21 @@ let createTiles = () => {
         }
       }
 
+      $tile.onmouseenter = e => {
+        $tile.classList.add('hovered');
+
+        getSurrounding($tile).forEach($t => $t.classList.add('surrounding'));
+      };
+
+      $tile.onmouseleave = e => {
+        $tile.classList.remove('hovered');
+
+        getSurrounding($tile).forEach($t => $t.classList.remove('surrounding'));
+      };
+
       $row.appendChild($tile);
     }
   }
-}
+};
 
 createTiles();
